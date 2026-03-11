aa7c37a384d5b4b3f64789f017c4f51f7cfd51ee
vrana <varunrana74@gmail.com>
2026-03-11 08:39:08 +0530

    feat: added get decorator

--- asgiapi/app.py
+++ asgiapi/app.py
@@ -1,7 +1,12 @@
 #a app class with asgi contract
 import json
+from asgiapi.router import Router
+from functools import wraps 
 
 class App:
+
+    def __init__(self):
+        self.router = Router()
 
     async def __call__(self,scope,receive,send):
         for i,(k,v) in enumerate(scope.items()):
@@ -25,3 +30,21 @@ class App:
             'type':'http.response.body',
             'body': response_body,
         })
+    
+    def get(self, path: str):
+
+        def decorator(func):
+
+            @wraps(func)
+            async def wrapper(*args, **kwargs):
+                return await func(*args, **kwargs)
+
+            self.router.add_route(
+                path_template=path,
+                method="GET",
+                handler=wrapper
+            )
+
+            return wrapper
+
+        return decorator
