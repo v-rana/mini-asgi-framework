5b311dbf59d72382e09a910bc81fe8aa64132b45
vrana <varunrana74@gmail.com>
2026-03-12 11:12:54 +0530

    fix: updated example

--- examples/main.py
+++ examples/main.py
@@ -1,10 +1,22 @@
 import uvicorn
 from asgiapi.app import App
 
-uvicorn_app = App()
+app = App()
 
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/users/{id}")
+async def get_user(*, id: int):
+    return {"user_id": id}
+
+# @app.get("/user/{id}")
+# def get_user(id:int):
+#     return f"user_{id}"
 
 if __name__ == "__main__":
-    uvicorn.run(uvicorn_app,host="127.0.0.1",port=8000)
+    uvicorn.run(app,host="127.0.0.1",port=8000)
     
