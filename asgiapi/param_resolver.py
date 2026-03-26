2fb842586c05a5fab02b2788d2e71d23f485afa3
vrana <varunrana74@gmail.com>
2026-03-26 09:40:06 +0530

    feat: added type conversion | annotaion based request injection

--- asgiapi/param_resolver.py
+++ asgiapi/param_resolver.py
@@ -2,9 +2,21 @@ import inspect
 
 
 class ParameterResolver:
-    #can init class get the request and path params here?
+    def __init__(self):
+        self.type_converters = {
+            int: int,
+            float: float,
+            str: str,
+            bool: self._to_bool
+        }
+    
+    def _to_bool(self,value):
+        return str(value).lower() in ("true","1","yes")
+
     def build_arguments(self,handler,request,path_params):
         bound_values = {}
+        if not isinstance(path_params, dict):
+            path_params = {}
         for name,param in inspect.signature(handler).parameters.items():
             value = self._resolve_param(
                 name,
@@ -18,18 +30,34 @@ class ParameterResolver:
         
     
     def _resolve_param(self,name,param,request,path_params):
-        if not isinstance(path_params, dict):
-            path_params = {}
-        #1. Inject Request , maybe make it more dynamic 
-        if name=="request":
+
+        #1. Inject Request 
+        if param.annotation.__name__ == "Request":
             return request
         #2. path params
         if name in path_params:
-            return path_params[name]
+            raw_value = path_params[name]
         #3. query params
-        if name in request.query_params:
-            return request.query_params[name]
-        if param.default is not  inspect.Parameter.empty:
+        elif name in request.query_params:
+            raw_value = request.query_params[name]
+        elif param.default is not  inspect.Parameter.empty:
             return param.default
+        else:
+            raise TypeError(f"Missing required parameter: {name}")
+
+        return self._convert_type(raw_value, param.annotation, name)        
+    
+    def _convert_type(self,value,annotation,name):
+
+        if annotation is inspect.Parameter.empty:
+            return value
         
-        raise TypeError(f"Missing required parameter: {name}")
+        converter = self.type_converters.get(annotation)
+
+        if converter is None:
+            return value
+        
+        try:
+            return converter(value)
+        except Exception as e:
+            raise TypeError(f"Invalid value for '{name}', expected {annotation.__name__}") from e
